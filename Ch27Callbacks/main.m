6d033c7dded70bf92369a9bf9fefb5472824439a
Xiao Lu <xiao.lu@c02gm58mdv7n-l.local>
2015-04-02 20:08:58 -0700

    Use block method for challenge 2

--- Ch27Callbacks/main.m
+++ Ch27Callbacks/main.m
@@ -15,18 +15,26 @@ int main(int argc, const char * argv[]) {
         BNRLogger *logger = [[BNRLogger alloc] init];
         
         // Callback type 3: Notification
-        [[NSNotificationCenter defaultCenter] addObserver:logger
-                                                 selector:@selector(zoneChange:)
-                                                     name:NSSystemTimeZoneDidChangeNotification
-                                                   object:nil];
+//        [[NSNotificationCenter defaultCenter] addObserver:logger
+//                                                 selector:@selector(zoneChange:)
+//                                                     name:NSSystemTimeZoneDidChangeNotification
+//                                                   object:nil];
+        
+        [[NSNotificationCenter defaultCenter] addObserverForName:NSSystemClockDidChangeNotification
+                                                          object:nil
+                                                           queue:nil
+                                                      usingBlock:^(NSNotification *note) {
+                                                          NSLog(@"The system time zone has changed!");
+                                                      }];
+        
 
         // Callback type 2: helper object or delegate
-        NSURL *url = [NSURL URLWithString:@"http://www.gutenberg.org/cache/epub/205/pg205.txt"];
-        NSURLRequest *request = [NSURLRequest requestWithURL:url];
-        
-        __unused NSURLConnection *fetchConn = [[NSURLConnection alloc] initWithRequest:request
-                                                                              delegate:logger
-                                                                      startImmediately:YES];
+//        NSURL *url = [NSURL URLWithString:@"http://www.gutenberg.org/cache/epub/205/pg205.txt"];
+//        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//        
+//        __unused NSURLConnection *fetchConn = [[NSURLConnection alloc] initWithRequest:request
+//                                                                              delegate:logger
+//                                                                      startImmediately:YES];
         
         // Callback type 1: target-action: timer
         /*
